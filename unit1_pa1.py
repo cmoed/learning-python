4946b030e3b5167f4c570b0c4ac82605b0be8994
cmoed <c.moed90@gmail.com>
2013-08-11 19:50:13 +0200

    Answering pa1 from unit1

---
+++ unit1_pa1.py
@@ -0,0 +1,14 @@
+# This is a Python comment. Lines that begin with a '#' are ignored by the
+# Python interpreter. Comments are useful for documenting code or explaining
+# quiz questions!
+
+# Write a Python program that prints out the number of minutes in seven weeks.
+# Click the "Run" button below to try running your code and see the output,
+# and click "Submit" to submit your answer.
+
+minutes_in_hour = 60
+hours_in_day = 24
+days_in_week = 7
+weeks = 7
+print minutes_in_hour * hours_in_day * days_in_week * weeks
+
